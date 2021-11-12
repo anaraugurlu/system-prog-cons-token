790ee8eacad97d4e19a1e71c7643f0cb86f089b8
ugur_ml97 <ugur_ml97@STHQ011A-07>
2021-11-12 18:13:46 +0400

    Add project files.

---
+++ system prog cons token/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using system_prog_cons_token.Commands;
+
+namespace system_prog_cons_token.ViewModels
+{
+    class MainViewModel:BaseViewModel
+    {
+
+        public RelayCommand StartButton { get; set; }
+        public RelayCommand ProgressBar { get; set; }
+        public RelayCommand CancelButton { get; set; }
+        public RelayCommand FileButton { get; set; }
+        public RelayCommand DecRadioButton { get; set; }
+        public RelayCommand EncRadioButton { get; set; }
+        public MainViewModel()
+        {
+            StartButton = new RelayCommand((sender) => { 
+            
+            });
+            CancelButton = new RelayCommand((sender) => {
+
+            });
+            FileButton = new RelayCommand((sender) => {
+
+            });
+           
+        }
+
+    }
+}
